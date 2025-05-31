2388555fbf15125a6f98a25494873eeef293ec0a
aorko01 <aorko321@gmail.com>
2025-05-31 10:33:54 +0600

    added logging and exception

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging 
+import os 
+from datetime import datetime
+
+Log_FILE = f"{datetime.now().strftime('%Y-%m-%d')}.log"
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_dir, Log_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    filemode='w'
+)
+
+
+    
+    
+
+
